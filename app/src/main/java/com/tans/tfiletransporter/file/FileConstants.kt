d4c4bd2cdf0f83434aa1de909311a766c12ff6ff
tans.tan <810555251@qq.com>
2021-01-26 21:21:00 +0800

    Format code.

--- app/src/main/java/com/tans/tfiletransporter/file/FileConstants.kt
+++ app/src/main/java/com/tans/tfiletransporter/file/FileConstants.kt
@@ -3,11 +3,12 @@ package com.tans.tfiletransporter.file
 import android.os.Environment
 import java.nio.file.FileSystem
 import java.nio.file.FileSystems
+import java.nio.file.Path
 import java.nio.file.Paths
 
 object FileConstants {
     val FILE_SYSTEM: FileSystem = FileSystems.getDefault()
     val FILE_SEPARATOR: String = FILE_SYSTEM.separator
-    val homePathString = Environment.getExternalStorageDirectory().path
-    val homePath = Paths.get(homePathString)
+    val homePathString: String = Environment.getExternalStorageDirectory().path
+    val homePath: Path = Paths.get(homePathString)
 }
