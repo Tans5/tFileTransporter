c49201888d407ace77331f9680214376018d2f75
pengcheng.tan <pengcheng.tan@fenrir-inc.com.cn>
2021-03-03 15:57:03 +0800

    Adjust tcp scan client timeout.

--- app/src/main/java/com/tans/tfiletransporter/net/connection/TcpScanConnection.kt
+++ app/src/main/java/com/tans/tfiletransporter/net/connection/TcpScanConnection.kt
@@ -142,10 +142,7 @@ class TcpScanConnectionClient(
                     val result = kotlin.runCatching {
                         val socket = Socket()
                         socket.use {
-                            socket.connect(
-                                InetSocketAddress(net, TCP_SCAN_CONNECT_LISTEN_PORTER),
-                                20
-                            )
+                            socket.connect(InetSocketAddress(net, TCP_SCAN_CONNECT_LISTEN_PORTER), 200)
                             val readChannel = Channels.newChannel(socket.getInputStream())
                             readChannel.readSuspendSize(byteBuffer = buffer, 4)
                             val deviceSize = buffer.asIntBuffer().get()
