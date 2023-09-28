7fc674f1d563478ff0f661ec3e22f2fc815a13cc
tanpengcheng <tanpengcheng@inke.cn>
2023-09-28 16:45:21 +0800

    [feat] Add video/audio fragment ui.

--- app/src/main/java/com/tans/tfiletransporter/ui/activity/filetransport/BaseMediaFragment.kt
+++ app/src/main/java/com/tans/tfiletransporter/ui/activity/filetransport/BaseMediaFragment.kt
@@ -1,6 +1,7 @@
 package com.tans.tfiletransporter.ui.activity.filetransport
 
 import android.os.Environment
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jakewharton.rxbinding4.swiperefreshlayout.refreshes
@@ -9,18 +10,22 @@ import com.tans.rxutils.QueryMediaType
 import com.tans.rxutils.getMedia
 import com.tans.rxutils.switchThread
 import com.tans.tadapter.adapter.DifferHandler
+import com.tans.tadapter.recyclerviewutils.MarginDividerItemDecoration
 import com.tans.tadapter.spec.SimpleAdapterSpec
 import com.tans.tadapter.spec.toAdapter
 import com.tans.tfiletransporter.R
 import com.tans.tfiletransporter.Settings
 import com.tans.tfiletransporter.databinding.BaseMediaFragmentLayoutBinding
 import com.tans.tfiletransporter.databinding.ImageItemLayoutBinding
+import com.tans.tfiletransporter.databinding.VideoAudioItemLayoutBinding
 import com.tans.tfiletransporter.logs.AndroidLog
 import com.tans.tfiletransporter.transferproto.fileexplore.FileExplore
 import com.tans.tfiletransporter.transferproto.fileexplore.requestSendFilesSuspend
 import com.tans.tfiletransporter.ui.activity.BaseFragment
 import com.tans.tfiletransporter.file.toFileExploreFile
+import com.tans.tfiletransporter.toSizeString
 import com.tans.tfiletransporter.transferproto.filetransfer.model.SenderFile
+import com.tans.tfiletransporter.utils.dp2px
 import io.reactivex.rxjava3.core.Single
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
@@ -44,7 +49,7 @@ abstract class BaseMediaFragment(
     }
 
     private val recyclerViewScrollChannel = Channel<Int>(1)
-    @Suppress("NAME_SHADOWING")
+
     override fun initViews(binding: BaseMediaFragmentLayoutBinding) {
 
         refreshMediaItems().switchThread().bindLife()
@@ -76,9 +81,9 @@ abstract class BaseMediaFragment(
                                 null
                             }
                         }),
-                    bindDataPayload = { _, data, binding, payloads ->
+                    bindDataPayload = { _, data, lBinding, payloads ->
                         if (payloads.contains(MediaItemSelectChange)) {
-                            binding.select = data.second
+                            lBinding.select = data.second
                             true
                         } else {
                             false
@@ -93,9 +98,66 @@ abstract class BaseMediaFragment(
             }
             else -> {
                 binding.myMediaItemsRv.layoutManager = LinearLayoutManager(requireContext())
-
-                // TODO
-                null
+                binding.myMediaItemsRv.addItemDecoration(
+                    MarginDividerItemDecoration.Companion.Builder()
+                        .divider(
+                            MarginDividerItemDecoration.Companion.ColorDivider(requireActivity().getColor(R.color.line_color),
+                                requireActivity().dp2px(1)))
+                        .marginStart(requireActivity().dp2px(65))
+                        .build()
+                )
+                SimpleAdapterSpec<Pair<QueryMediaItem, Boolean>, VideoAudioItemLayoutBinding>(
+                    layoutId = R.layout.video_audio_item_layout,
+                    bindData = { _, (item, isSelect), lBinding ->
+                        lBinding.mediaCb.isChecked = isSelect
+                        if (item is QueryMediaItem.Video) {
+                            lBinding.mediaItemIv.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_movie))
+                            lBinding.titleTv.text = item.displayName
+                            lBinding.artistTv.text = requireContext().getString(R.string.media_artist_name, item.artist)
+                            lBinding.albumTv.text = requireContext().getString(R.string.media_album_name, item.album)
+                            lBinding.mediaSizeTv.text = item.size.toSizeString()
+                        }
+                        if (item is QueryMediaItem.Audio) {
+                            lBinding.mediaItemIv.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_audio))
+                            lBinding.titleTv.text = item.displayName
+                            lBinding.artistTv.text = requireContext().getString(R.string.media_artist_name, item.artist)
+                            lBinding.albumTv.text = requireContext().getString(R.string.media_album_name, item.album)
+                            lBinding.mediaSizeTv.text = item.size.toSizeString()
+                        }
+                    },
+                    itemClicks = listOf { lBinding, _ ->
+                        lBinding.root to { _, (item, lastSelectStatus) ->
+                            selectMediaItem(!lastSelectStatus, item).map { Unit }
+                        }
+                    },
+                    dataUpdater = bindState().map { state ->
+                        state.mediaItems.filter { it is QueryMediaItem.Video || it is QueryMediaItem.Audio }.map {
+                            it to state.selectedMediaItems.contains(it)
+                        }
+                    },
+                    differHandler = DifferHandler(itemsTheSame = { d1, d2 -> d1.first.uri == d2.first.uri },
+                        contentTheSame = { d1, d2 -> d1.first.uri == d2.first.uri && d1.second == d2.second },
+                        changePayLoad = { d1, d2 ->
+                            if (d1.first.uri == d2.first.uri && d1.second != d2.second) {
+                                MediaItemSelectChange
+                            } else {
+                                null
+                            }
+                        }),
+                    bindDataPayload = { _, data, lBinding, payloads ->
+                        if (payloads.contains(MediaItemSelectChange)) {
+                            lBinding.mediaCb.isChecked = data.second
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                ).toAdapter {
+                    val position = recyclerViewScrollChannel.tryReceive().getOrNull()
+                    if (position != null && it.isNotEmpty()) {
+                        (binding.myMediaItemsRv.layoutManager as? LinearLayoutManager)?.scrollToPositionWithOffset(position, 0)
+                    }
+                }
             }
         }
 
