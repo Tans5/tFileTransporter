1734b4c556f425fabc0df6c69292366df37073b6
tans.tan <tans.tan096@gmail.com>
2024-03-30 19:17:20 +0800

    [fix] Fix compile error.

--- net/build.gradle.kts
+++ net/build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
-    id("java-library")
     id("com.google.devtools.ksp")
     alias(libs.plugins.jetbrainsKotlinJvm)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
+//java {
+//    sourceCompatibility = JavaVersion.VERSION_1_8
+//    targetCompatibility = JavaVersion.VERSION_1_8
+//}
 
 dependencies {
      api(libs.coroutines.core)
